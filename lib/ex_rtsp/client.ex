c81b96148f50ca805b8178a32c34ac624b39dad0
shavit <shavit@users.noreply.github.com>
2020-08-17 15:16:00 -0400

    Add a teardown

--- lib/ex_rtsp/client.ex
+++ lib/ex_rtsp/client.ex
@@ -123,6 +123,22 @@ defmodule ExRtsp.Client do
     {:reply, res, state}
   end
 
+  def handle_call({:teardown, opts}, _ref, state) do
+    url = build_url(state) <> "/trackID=1"
+
+    req =
+      Request.new(
+        url: url,
+        cseq: state.cseq + 1,
+        method: :record,
+        session: state.session_id
+      )
+
+    res = send_req(state.conn, req)
+
+    {:reply, res, state}
+  end
+
   def handle_call({:send_req, req}, _ref, state) do
     state = %{state | cseq: state.cseq + 1}
 
