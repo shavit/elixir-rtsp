4710847065ee888ad9e6fbcf1e789bae51813cee
shavit <shavit@users.noreply.github.com>
2020-08-21 23:40:02 -0400

    add client listener

---
+++ lib/ex_rtsp/client/rtp.ex
@@ -0,0 +1,29 @@
+defmodule ExRtsp.Client.RTP do
+  @moduledoc """
+  Documentation for `ExRtsp.Client.RTP`.
+  """
+  use GenServer
+  require Logger
+
+  def start_link(opts) do
+    name = Keyword.get(opts, :name, __MODULE__)
+    GenServer.start_link(__MODULE__, opts, name: name)
+  end
+
+  def init(opts) do
+    port = Keyword.get(opts, :port, 554)
+    {:ok, socket} = :gen_tcp.listen(port, [:binary, {:active, true}])
+
+    state = %{
+      port: port,
+      socket: socket
+    }
+
+    {:ok, state}
+  end
+
+  def handle_info({:tcp, _from, msg}, state) do
+    Logger.info("[Client.RTP] New message: #{inspect(msg)}")
+    {:noreply, state}
+  end
+end
