6b9e100f68b0dd1610c8fa222c4b13e93fe9410c
shavit <shavit@users.noreply.github.com>
2020-08-28 18:54:29 -0400

    add default encoder

---
+++ lib/ex_rtsp/encoder/ffmpeg.ex
@@ -0,0 +1,5 @@
+defmodule ExRtsp.Encoder.Ffmpeg do
+  @moduledoc """
+  Documentation for `ExRtsp.Encoder.Ffmpeg`.
+  """
+end
---
+++ test/ex_rtsp/encoder/ffmpeg_test.exs
@@ -0,0 +1,7 @@
+defmodule ExRtsp.Encoder.FfmpegTest do
+  alias ExRtsp.Encoder.Ffmpeg
+  use ExUnit.Case
+
+  describe "ffmpeg" do
+  end
+end
