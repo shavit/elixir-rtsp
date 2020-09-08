0647a78b6959ff21c5fcd5cf2d2851c502d1ebef
shavit <shavit@users.noreply.github.com>
2020-09-08 07:00:00 -0400

    test setup

--- test/ex_rtsp/encoder/ffmpeg_test.exs
+++ test/ex_rtsp/encoder/ffmpeg_test.exs
@@ -3,5 +3,9 @@ defmodule ExRtsp.Encoder.FfmpegTest do
   use ExUnit.Case
 
   describe "ffmpeg" do
+    test "setup/1 creates a command" do
+      assert {:ok, port} = Ffmpeg.setup(job_id: 11)
+      :ok = File.rm!("/tmp/ffmpeg_socket_11")
+    end
   end
 end
