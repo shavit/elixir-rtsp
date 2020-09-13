2ad6dbc4927ec979f4ee8d191df1fa1c6a8c557d
shavit <shavit@users.noreply.github.com>
2020-09-13 04:55:21 -0400

    update test

--- test/ex_rtsp/encoder/ffmpeg_test.exs
+++ test/ex_rtsp/encoder/ffmpeg_test.exs
@@ -10,7 +10,8 @@ defmodule ExRtsp.Encoder.FfmpegTest do
     end
 
     test "teardown/2 remove artifacts" do
-      assert {:ok, port, filename} = Ffmpeg.setup(job_id: 11)
+      job_id = 11
+      assert {:ok, port, filename} = Ffmpeg.setup(job_id: job_id)
       assert true = Ffmpeg.teardown(port, 11)
       assert false == File.exists?("/tmp/ffmpeg_job_#{job_id}")
       assert false == File.exists?(filename)
