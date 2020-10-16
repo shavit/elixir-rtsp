7353120e093fc5e10a3df014981a712c413a4ede
shavit <shavit@users.noreply.github.com>
2020-10-15 23:04:08 -0400

    remove name

--- lib/ex_rtsp/rtp.ex
+++ lib/ex_rtsp/rtp.ex
@@ -66,7 +66,6 @@ defmodule ExRtsp.RTP do
   @payload_types payload_types
 
   def start_link(opts) do
-    #name = Keyword.get(opts, :name, __MODULE__)
     GenServer.start_link(__MODULE__, opts)
   end
 
