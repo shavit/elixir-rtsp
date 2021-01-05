9e6136b84c949791ad58185428df5c94d357173e
shavit <shavit@users.noreply.github.com>
2021-01-05 08:44:30 -0500

    Test call properties

--- test/ex_rtsp/client_test.exs
+++ test/ex_rtsp/client_test.exs
@@ -15,5 +15,37 @@ defmodule ExRtsp.ClientTest do
               {:continue, :dial_rtsp}} =
                Client.init(abs_path: "/stream-1", host: "0.0.0.0", port: 4000)
     end
+
+    test "handle_continue/2 handle connection error" do
+      state = %{
+        host: "127.0.0.1",
+        port: 554,
+        conn: nil
+      }
+
+      assert {:noreply, state} = Client.handle_continue(:dial_rtsp, state)
+      assert "127.0.0.1" == state.host
+      assert 554 == state.port
+      assert nil == state.conn
+    end
+
+    test "handle_call/3 describe" do
+      state = %{
+        abs_path: "/stream-test",
+        cseq: 1,
+        host: "127.0.0.1",
+        port: 554,
+        conn: nil
+      }
+
+      opts = []
+      ref = "some ref"
+      assert {:reply, res, state} = Client.handle_call({:describe, opts}, ref, state)
+      assert {:error, "need to reconnect"} == res
+      assert nil == state.conn
+      assert 1 == state.cseq
+      assert "127.0.0.1" == state.host
+      assert 554 == state.port
+    end
   end
 end
